60e1ed5e153b7383ef91fd2ebacd438cddf7cafb
carmencng94 <karmencng@gmail.com>
2026-03-03 15:08:03 +0100

    fix: eliminar conflictos de fusión en el archivo database.js

--- bck2_carmen/database.js
+++ bck2_carmen/database.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
     export const productos = [
   { id: 'A001', titulo: 'Monitor 27', precio: 300 },
   { id: 'A002', titulo: 'CPU-Zi', precio: 100 },
   { id: 'A003', titulo: 'Microfono', precio: 24 },
   { id: 'A004', titulo: 'Teclado mecánico', precio: 55 },
   { id: 'A005', titulo: 'Webcam HD', precio: 45 }
-=======
-// database.js
-// Este archivo simula nuestra "base de datos" en memoria
-// Lo exportamos para poder usarlo desde otros archivos
-
-export const productos = [
-  { id: 'A001', titulo: 'Monitor 27',     precio: 300 },
-  { id: 'A002', titulo: 'CPU-Zi',         precio: 100 },
-  { id: 'A003', titulo: 'Microfono',      precio:  24 },
-  { id: 'A004', titulo: 'Teclado mecánico', precio: 55 },
-  { id: 'A005', titulo: 'Webcam HD',      precio: 45 }
->>>>>>> e5a6ba341c87df80b69d2644ea3d205abdf5ae5d
 ];
